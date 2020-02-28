f0b0758bb30d3dd7f72d15257602c33f318f8630
Ruslan Radko <rradko@ergoserv.com>
2020-02-28 18:35:40 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  Publisher.create!(
+    name: Faker::Name.name
+  )
+end
+
+10.times do
+  author = Author.create!(
+    name: Faker::Name.name
+  )
+
+  10.times do
+    book = Book.create!(
+      publisher: Publisher.all.sample,
+      title: Faker::Book.title,
+      isbn: Faker::Code.isbn,
+      publishing_year: Faker::Number.between(1700,2020)
+    )
+    book.authors << author
+  end
+end
+
+puts 'Seed finished'
+puts "#{Publisher.count} lists created"
+puts "#{Author.count} items created"
+puts "#{Book.count} items created"
